448e0523ba46d5e991832cf85432d35272aab2a0
Luka Ivanyshyn <luka.ivanyshyn@lehre.roche.com>
2025-11-24 16:40:26 +0100

    Break tests on purpose

--- index.test.js
+++ index.test.js
@@ -5,8 +5,8 @@ import { sum } from './index.js'
 
 describe('sum', () => {
   it('should add 1 + 2 to equal 3', () => {
-    assert.equal(sum(1, 2), 3)
-  })
+  assert.equal(sum(1, 2), 4) // спеціально неправильне очікування
+})
 
   it('should add 1.5 + 2.25 to equal 3.75', () => {
     assert.equal(sum(1.5, 2.25), 3.75)
